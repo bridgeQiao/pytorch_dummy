b4e46fa26526c6730be04f24c2d080ae93f9db03
JoeW”CORRECT_EMAIL=“wq839458310@163.com <>
2024-07-29 08:18:16 +0800

    change to pybind11

--- torch_dpu/csrc/init_dpu_bindings.cpp
+++ torch_dpu/csrc/init_dpu_bindings.cpp
@@ -1,37 +1,12 @@
-#include <Python.h>
-#include <vector>
+#include <cstdio>
+#include <pybind11/pybind11.h>
 
-PyObject *module;
-static std::vector<PyMethodDef> methods;
+namespace py = pybind11;
 
-void AddPyMethodDefs(std::vector<PyMethodDef> &vector, PyMethodDef *methods) {
-  if (!vector.empty()) {
-    // remove nullptr terminator
-    vector.pop_back();
-  }
-  while (true) {
-    vector.push_back(*methods);
-    if (!methods->ml_name) {
-      break;
-    }
-    methods++;
-  }
+void print_hello() {
+    printf("Hello\n");
 }
 
-PyObject *hello(PyObject *) {
-  printf("[Info] hello world.\n");
-  Py_RETURN_NONE;
+PYBIND11_MODULE(_C, m) {
+    m.def("pirnt_hello", &print_hello, "A function that prints 'hello'");
 }
-static PyMethodDef TorchDpuHello[] = {
-    {"_dpu_hello", (PyCFunction)hello, METH_NOARGS, nullptr},
-    {nullptr, nullptr, 0, nullptr}};
-
-extern "C" PyObject *initModule() {
-  AddPyMethodDefs(methods, TorchDpuHello);
-  static struct PyModuleDef torchdpu_module = {
-      PyModuleDef_HEAD_INIT, "torch_dpu._C", nullptr, -1, methods.data()};
-  module = PyModule_Create(&torchdpu_module);
-  return module;
-}
-
-PyMODINIT_FUNC PyInit__C(void) { return initModule(); }
